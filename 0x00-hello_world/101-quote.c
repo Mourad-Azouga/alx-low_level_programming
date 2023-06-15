9c6d29bd7d0d4fbba09ed7c33863b0d17a2c0e35
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-06-15 07:02:28 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - Entry point
- *
- * Return: Always 1 (Success)
- */
+
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, message, sizeof(message) - 1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
+

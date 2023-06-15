059129217d68d51a5c5392d0b3916f001f785742
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-06-15 06:38:11 -0700

    Fixed 101 for betty issues

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
-
-int main() {
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void) {
     char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(1, message, sizeof(message) - 1);
     return 1;

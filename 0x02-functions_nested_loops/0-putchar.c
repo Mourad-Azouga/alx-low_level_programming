cc1f58d2d41429327b5805bd8dda69f7bb185a70
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-06-20 04:39:40 -0700

    Changes made or files added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,6 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- *
- * Return: On success, returns the character written.
- * On error, returns -1 and sets errno appropriately.
- */
-/**
  * main - Entry point of the program
  *
  * Description: This is the main function that serves as the

32569c5a988a4b45b1e92ab5e36e7164613aed10
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-06-20 16:09:50 -0700

    Changes made or files added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the character sign
+ * @c: The character to be checked.
+ * Description: This function tests if the character
+ *              is negative positive of equals 0 and
+ *              returns and prints the result
+ * Return: 1 if true 0 if false
+ */
+int print_sign(int c)
+{
+if (c > 0)
+{
+return (1);
+_putchar('+');
+}
+if (c < 0)
+{
+return (-1);
+_putchar('-');
+}
+else
+{
+return (0);
+_putchar('0');
+}
+}

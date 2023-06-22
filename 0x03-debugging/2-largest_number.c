a74a316076917e889498cc9b7c602e44b9b681da
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-06-22 03:36:45 -0700

    Added or changed stuff

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,15 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (a > b && c > a)
-{
-largest = c;
-}
 else if (b > c)
 {
-	largest = b;
+largest = b;
 }
-	else
+else
 {
 largest = c;
 }

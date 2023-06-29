7b3a18b3766e8021d6b892bd98de8685302f18a5
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-06-29 14:49:26 -0700

    Fixed 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,34 +12,11 @@
  *   negative integer if s1 is less than s2
  */
 int _strcmp(char *s1, char *s2)
-{
-int result = 0;
-int len1, len2;
+	{
 int i = 0;
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-while (i < len1 && i < len2)
+while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 {
-if (s1[i] > s2[i])
-{
-result = 15;
-break;
-}
-else
-if (s1[i] < s2[i])
-{
-result = -15;
-break;
-}
 i++;
 }
-if (result == 0)
-{
-if (len1 > len2)
-result = 15;
-else
-if (len1 < len2)
-result = -15;
-}
-return (result);
+return (s1[i] - s2[i]);
 }

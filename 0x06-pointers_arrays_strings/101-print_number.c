8be9eabd7fc7e39906a8fa2b31185575fa76fc6f
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-06-30 08:33:25 -0700

    Fixed 101-number_print god help me

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,15 +3,18 @@
 /**
  * print_number - Prints an integer
  * @n: The integer to be printed
+ * return: void
  */
 void print_number(int n)
 {
+unsigned int n1;
+n1 = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+n1 = -n;
 }
-if ((n / 10) != 0)
-print_number(n / 10);
-_putchar('0' + (n % 10));
+if (n1 / 10 != 0)
+print_number(n1 / 10);
+_putchar('0' + (n1 % 10));
 }

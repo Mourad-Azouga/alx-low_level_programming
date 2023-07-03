669b2f5e48b2512c45eb7537af9a0497a40a22f6
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-03 15:11:11 -0700

    Added dir 7 with its README and code 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+
+/**
+ * *_memset - fills the first n bytes of s with the cte b
+ * @n: memory area
+ * @s: pointer to the work area
+ * @b: the cte that s will be filled with.
+ * Return: the pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+if (s == NULL || n == 0)
+return (s);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}

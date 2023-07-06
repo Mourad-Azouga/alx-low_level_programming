c2702ad4f1590a8b63b61ae40188e5dac6df4ac2
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-06 04:22:53 -0700

    Added or changed some code

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@ int sqrt_helper(int n, int i);
  * _sqrt_recursion - Calculates the square root of a number.
  * @n: The number to calculate the square root of.
  *
- * Return: The square root of n, or -1 if it does not have a natural square root.
+ * Return: The sqrt of n, or -1 if it does not have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
@@ -23,7 +23,7 @@ return (sqrt_helper(n, 1));
  * @n: The number to calculate the square root of.
  * @i: The current guess for the square root.
  *
- * Return: The square root of n, or -1 if it does not have a natural square root.
+ * Return: The sqrt of n, or -1 if it does not have a natural square root.
  */
 int sqrt_helper(int n, int i)
 {

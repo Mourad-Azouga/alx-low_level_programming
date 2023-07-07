c39a78237a012d7d13da11bb7eb2f4443525f9a8
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-07 09:37:25 -0700

    Fixed stuff I guess

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main (int __attribute__((unused)) argc, char *argv[])
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array of strings containing the command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
-return(0);
+return (0);
 }

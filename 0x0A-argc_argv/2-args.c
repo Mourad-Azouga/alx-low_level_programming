35b48e17dd3170d778832a38332be59ad78a3f7a
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-07 10:44:37 -0700

    Added or fixed stuff

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
 #include"main.h"
 /**
- * main - Entry point of the program
- * @argc: The number of command line arguments
- * @argv: An array of strings containing the command line arguments
- *
- * Return: Always 0 (Success)
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
  */
 int main(int argc, char *argv[])
 {
 int count;
-printf("This program was called with \"%s\".\n", argv[0]);
-if (argc > 1)
-{
 for (count = 1; count < argc; count++)
-{
-printf("argv[%d] = %s\n", count, argv[count]);
-}
-}
-else
-{
-printf("The command had no other arguments.\n");
-}
+printf("%s\n", argv[count]);
 return (0);
 }

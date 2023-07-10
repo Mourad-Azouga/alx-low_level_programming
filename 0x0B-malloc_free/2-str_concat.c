9fd99a9ac786da9a3bdd7f575524c24785061523
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-10 15:43:48 -0700

    This commit is made by an automatic pusher

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Pointer to the concatenated string or NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *concat;
+unsigned int i, j, len1, len2;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+concat = malloc((len1 + len2 + 1) * sizeof(char));
+if (concat == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+concat[i] = s1[i];
+for (j = 0; j < len2; j++)
+concat[i++] = s2[j];
+concat[i] = '\0';
+return (concat);
+}

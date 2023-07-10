0379c84eb18406168581ab920ce1c6325ae1e2f0
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-10 06:41:23 -0700

    Added 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_numeric(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * is_numeric - Checks if a string is a numeric value
+ * @str: The string to check
+ *
+ * Return: 1 if numeric, 0 otherwise
+ */
+int is_numeric(char *str)
+{
+	int i = 0;
+
+	if (str[0] == '-')
+		i++;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+
+	return (1);
+}
+

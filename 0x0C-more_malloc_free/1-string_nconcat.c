7a26c0fe69e243f44a71d097aface3498f7d84f2
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-13 07:53:36 -0700

    This is an automatic pusher

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "strlen.c"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: number of bytes of the second string
+ * Return: Pointer to the concatenated string or NULL on failure.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *concat;
+unsigned int i, j, len1, len2, k;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+concat = malloc((len1 + len2 + 1) * sizeof(char));
+
+if (concat == NULL)
+return (NULL);
+
+if (n >= len2)
+k = len2;
+else
+k = n;
+
+for (i = 0; i < len1; i++)
+concat[i] = s1[i];
+for (j = 0; j < k; j++)
+concat[i++] = s2[j];
+concat[i] = '\0';
+return (concat);
+}

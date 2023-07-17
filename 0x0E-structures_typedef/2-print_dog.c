86c852650b33a224924906487f4340162125653a
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-17 13:10:25 -0700

    This is an automatic pusher

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,22 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
-
 /**
- * print_dog - Prints dog structure.
- * @d: pointer to structure
-*/
+ * print_dog - prints dog variables
+ * @d: struct of dog
+ */
 void print_dog(struct dog *d)
 {
-if (d != NULL)
-{
-if (d->name == NULL)
-printf("Name: (nil)\n");
-else
-printf("Name: %s\n", d->name);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 
-printf("Age: %.1f\n", d->age);
-
-if (d->owner == NULL)
-printf("Owner: (nil)\n");
-else
-printf("Owner: %s\n", d->owner);
-}
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }

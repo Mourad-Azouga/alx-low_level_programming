62460571c7a81ff4692d5a0463adfe8eec61b03b
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-17 08:04:56 -0700

    This is an automatic pusher

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
  * struct dog - Structure.
  * @name: name
@@ -16,4 +16,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+#endif /*_DOG_H_ */

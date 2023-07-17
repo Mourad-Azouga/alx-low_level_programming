153b4b0850feb008f9295ed7c43c03a5f34ba8e3
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-17 06:19:33 -0700

    This is an automatic pusher

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#ifndef MAIN_C
+#define MAIN_C
+#include <stdio.h>
+#define PRINT_FILENAME printf("%s\n", __FILE__)
+
+/**
+ * main - Main program.
+ * Return: 0 Success
+*/
+int main(void)
+{
+PRINT_FILENAME;
+return (0);
+}
+#endif

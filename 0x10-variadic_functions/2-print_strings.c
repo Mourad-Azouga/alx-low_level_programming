640a424ecc8185c4046d1e5461a645d01161ea38
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-20 08:52:49 -0700

    This is an automatic pusher

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,18 +16,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 va_list lst;
 unsigned int i;
 char *z;
-if (n != 0)
+if (n > 0)
 {
 va_start(lst, n);
 for (i = 0; i < n; i++)
 {
 z = va_arg(lst, char*);
+if (separator != NULL && i > 0)
+printf("%s", separator);
 if (z == NULL)
 printf("(nil)");
 else
 printf("%s", z);
-if (separator != NULL && i != n - 1)
-printf("%s", separator);
 }
 printf("\n");
 va_end(lst);

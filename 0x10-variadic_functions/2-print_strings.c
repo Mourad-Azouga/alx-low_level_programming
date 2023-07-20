392aaf142cfe5b4c4d93f1aa43c0b96e01dc1150
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-20 08:54:24 -0700

    This is an automatic pusher

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,33 +3,30 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-
 /**
  * print_strings - Prints strings
  * @separator: The string between strings.
  * @n: Number of arguments
  * Return: Void
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list lst;
-unsigned int i;
-char *z;
-if (n > 0)
-{
-va_start(lst, n);
-for (i = 0; i < n; i++)
-{
-z = va_arg(lst, char*);
-if (separator != NULL && i > 0)
-printf("%s", separator);
-if (z == NULL)
-printf("(nil)");
-else
-printf("%s", z);
-}
-printf("\n");
-va_end(lst);
-}
+	unsigned int i;
+	va_list stringArgs;
+	char *strArg;
+
+	va_start(stringArgs, n);
+	for (i = 0; i < n; i++)
+	{
+		strArg = va_arg(stringArgs, char *);
+
+		if (separator != NULL && i > 0)
+			printf("%s", separator);
+		if (strArg == NULL)
+			printf("(nil)");
+		else
+			printf("%s", strArg);
+	}
+	va_end(stringArgs);
+	printf("\n");
 }

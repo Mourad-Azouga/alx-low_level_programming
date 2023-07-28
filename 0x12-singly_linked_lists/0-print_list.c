835ba73d47754d8b432807eabc8011f673fa28c2
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-28 03:24:12 -0700

    This is an automated pusher

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,15 @@
 
 size_t print_list(const list_t *h)
 {
-const list_t *current = h;
 size_t count = 0;
-while (current != NULL)
-{
-{
-if (current->str != NULL)
-printf("[%u] %s\n", current->len, current->str);
+while (h != NULL)
+	{
+if (h->str != NULL)
+printf("[%u] %s\n", h->len, h->str);
 else
 printf("[0] (nil)\n");
-
+h = h->next;
 count++;
-current = current->next;
-}
 }
 return (count);
 }

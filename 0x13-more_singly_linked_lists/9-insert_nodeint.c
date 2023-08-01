41438e2bd6f6f4842e4d8f57b144e2c3b9d36e79
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-08-01 13:02:25 -0700

    Fixed betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,17 +4,17 @@
 /**
  * insert_nodeint_at_index - Adds node at index
  * @head: head of node linked list
- * @idx: index where node is added 
+ * @idx: index where node is added
  * @n: node's data
  * Return: The sum.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        listint_t *temp = *head, *new_node = malloc(sizeof(listint_t));
+	listint_t *temp = *head, *new_node = malloc(sizeof(listint_t));
 	unsigned int i = 0;
 
-        if (new_node == NULL)
-        return (NULL);
+if (new_node == NULL)
+return (NULL);
 new_node->n = n;
 if (idx == 0)
 {
@@ -23,7 +23,7 @@ new_node->next = *head;
 return (new_node);
 }
 
-	for (i = 0; i < idx -1 && temp != NULL; i++)
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
 	temp = temp->next;
 
 if (temp == NULL)

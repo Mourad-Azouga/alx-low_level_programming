f12c12ad233d492f707309409a1591d63828bcfa
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-08-09 14:44:17 +0100

    Made it so it doesnt shut down instantly

--- shell_playground/3-read_line.c
+++ shell_playground/3-read_line.c
@@ -1,32 +1,42 @@
-       #define _GNU_SOURCE
-       #include <stdio.h>
-       #include <stdlib.h>
-       #include <string.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char *strtok(char *str, const char *delim);
 
-    void main(int argc, char *argv[])
-       {
-        int d;
-        size_t n;
-        char *buffer, *toktok = NULL;
-    // Reads the line 
-           printf("$ ");
-           getline(&buffer, &n, stdin);
-    // Devides the line into words
-           toktok = strtok(buffer, " ");
+int main(int argc, char *argv[]) {
+    int d = 1;
+    size_t n = 0; // Initialize n to 0
+    char *buffer = NULL; // Initialize buffer to NULL
+    char *toktok = NULL;
+
+    // Reads the line
+    printf("$ ");
+    getline(&buffer, &n, stdin);
+
+    // Divides the line into words
+    toktok = strtok(buffer, " ");
+
     // Prints the line
-           while (toktok != NULL)
-           {
-           printf("%s\n", toktok);
-           toktok = strtok(NULL, " ");
-           }          
-          
-           printf("Press 0 to exit");
-           scanf("%d", &d);
-           if (d = 0)
-           exit(EXIT_SUCCESS);
-           else
-           exit(EXIT_FAILURE);
-       }
+    while (toktok != NULL) {
+        printf("%s", toktok);
+        toktok = strtok(NULL, " ");
+    if (toktok != NULL)
+        printf("\n");
+    }
+
+    // Added code so it doesn't instantly shut down in exe
+    if (d == 1) { // Use == for comparison
+        printf("Input anythig to exit\n");
+        scanf("%d", &d);
+    } else {
+        exit(EXIT_SUCCESS);
+    }
+
+    // Free allocated memory
+    free(buffer);
+
+    return 0;
+}

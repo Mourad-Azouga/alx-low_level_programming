7641d92e6f8658aeb2440562bd0ebd8468c0ced3
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-08-11 15:17:34 +0100

    moved the child printf to under execve

--- shell_playground/exec.c
+++ shell_playground/exec.c
@@ -17,14 +17,14 @@ int main(void)
     {
         if(fork() == 0)
         {
-            // To know which child is on
-            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+            
             
     if (execve(argv[0], argv, NULL) == -1)
     {
         perror("Error:");
     }
-    
+    // To know which child is on
+            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
         }
     }    
     for(int i=0;i<5;i++) // loop will run n times (n=5)

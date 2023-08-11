8055642d1d261d3cfaf3f60c99a4e359d5807a28
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-08-11 15:33:25 +0100

    Changed the script

--- shell_playground/exec.c
+++ shell_playground/exec.c
@@ -22,16 +22,16 @@ int main(void)
     if (execve(argv[0], argv, NULL) == -1)
     {
         perror("Error:");
-    }    // To know which child is on
-            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
-    printf("After execve\n");
-    
+    }
     }    
     for(int i=0;i<5;i++) // loop will run n times (n=5)
-    wait(NULL);
-    
+    {
 
->>>>>>> cfd574d61603382c0d683189b8ed354bdc5bedc2
+        // To know which child is on
+    printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+    wait(NULL);
+    }
+
     return (0);
 }
 

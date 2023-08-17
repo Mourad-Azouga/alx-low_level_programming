d487ee1300b6d2be4d86be9f4d641edb33cc909e
Mourad Azouga <mourad-azouga@outlook.fr>
2023-08-17 12:45:40 +0100

    m

--- shell_playground/printenv.c
+++ shell_playground/printenv.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
+
+extern char **environ;
 /*
  * main - Main program
  * @ac: number of args
  * @av: arg holder
  * @env: environment
  */
-int main(int ac, char **av, char **env)
-{
 
+int main() {
+    char **env = environ;
 
+    while (*env != NULL) {
+        printf("%s\n", *env);
+        env++;
+    }
 
+    return 0;
 }
+

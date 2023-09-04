1d5bf5c6d38b6af7d4cfa9687db20337f079656a
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-09-04 08:39:17 -0700

    Added 2 but Im a bit skeptical that itll work

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <unistd.h>
+/*
+ * append_text_to_file - Adds the desired text to the existing file
+ * @filename: file to be operating with
+ * @text_content: text to be added
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp;
+	ssize_t byte;
+	int len = 0;
+
+if (!filename)
+	return (-1);
+
+fp = open(filename, O_WRONLY | O_APPEND);
+if (fp == -1)
+	return (-1);
+/*While loop to get length instead of _strlen*/
+while (!text_content)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+
+byte = write(fp, text_content, len);
+if (byte == -1)
+	return (-1);
+
+close(fp);
+return (1);
+}

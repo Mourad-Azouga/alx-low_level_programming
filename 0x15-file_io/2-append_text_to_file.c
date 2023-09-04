a6785122496403ceeae567c043eeb598cd8b7453
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-09-04 08:55:48 -0700

    Changed the code entirely to be betty friendly

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,41 @@
 #include "main.h"
 #include <unistd.h>
-/*
- * append_text_to_file - Adds the desired text to the existing file
- * @filename: file to be operating with
- * @text_content: text to be added
- * Return: 1 on success and -1 on failure
+/**
+ * _strlen - length of str
+ * @b: The str
+ * Return: len
+ */
+int _strlen(char *b)
+{
+int i = 0;
+if (!b)
+return (0);
+
+while (*b++)
+i++;
+
+return (i);
+}
+
+
+
+/**
+ * append_text_to_file - Adds text to existing file
+ * @filename: The file name
+ * @text_content: Text cnt
+ * Return: 1 if success and -1 if faillure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fp;
-	ssize_t byte;
-	int len = 0;
-
+int fd;
+ssize_t bytes = 0, len = _strlen(text_content);
 if (!filename)
-	return (-1);
-
-fp = open(filename, O_WRONLY | O_APPEND);
-if (fp == -1)
-	return (-1);
-if (text_content)
-{
-		for (len = 0; text_content[len]; len++){}
-	
-
-byte = write(fp, text_content, len);
-if (byte == -1)
-{
-	close(fp);
-	return (-1);
-}
-}
-close(fp);
-return (1);
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
+if (len)
+bytes = write(fd, text_content, len);
+close(fd);
+return (bytes == len ? 1 : -1);
 }

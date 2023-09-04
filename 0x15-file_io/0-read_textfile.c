2c47c47f05f2ff5a31108fb0d6ced0296a96688f
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-09-04 06:55:22 -0700

    Fixed the read_textfile program for the 4th time, should work now

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,39 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <unistd.h>
 /**
- * read_textfile - Reads a text file and prints it to stdout.
- * @filename: The filename.
- * @letters: The number of letters to be read and printed.
- * Return: The actual number of letters read and printed, or -1 on failure.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *fp;
-    ssize_t bytes_read, bytes_written;
-    char buffer[1024 * 8];
+	ssize_t o, r, w;
+	char *buffer;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (0);
 
-    fp = fopen(filename, "r");
-    if (fp == NULL)
-        return (-1);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    bytes_read = fread(buffer, 1, letters, fp);
-    if (bytes_read == 0)
-    {
-        fclose(fp);
-        return (0); 
-    }
-    else if (bytes_read == -1)
-    {
-        fclose(fp);
-        return (-1); 
-    }
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-    bytes_written = fwrite(buffer, 1, bytes_read, stdout);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    fclose(fp);
-    return (bytes_written);
+	free(buffer);
+	close(o);
+
+	return (w);
 }
-

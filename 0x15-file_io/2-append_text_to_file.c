1f7d3ef5e41b6cf8ace98499a6258914d80b3418
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-10-02 15:44:55 -0700

    Fixed 1 and added 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * append_text_to_file - appens text at the end of a file
+ * @filename:  is the name of the file
+ * @text_content:  is the NULL terminated string to add at the end of the file
+ * Return: 1 success -1 failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t b_written = 0;
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+
+	if (b_written < 0)
+		return (-1);
+	return (1);
+}

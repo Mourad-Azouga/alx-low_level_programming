c251a895bb090722930c060fe65204b0ac90dd93
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-11-08 23:31:34 -0800

    Added new dir and stuff

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		len++;
+		h = h->next;
+	}
+return (len);
+}
